e3518935b6ec06f3857d3828611e63f1a755b219
kis <vickyorlo@gmail.com>
2019-09-07 22:07:08 +0200

    big update

--- neural_network.py
+++ neural_network.py
@@ -1,127 +1,110 @@
-from keras.layers import Conv2D, UpSampling2D, Input, MaxPooling2D, BatchNormalization
-from keras.models import Model, load_model
+from keras.layers import Conv1D, UpSampling1D, Input, MaxPooling1D, BatchNormalization, Dense, Flatten,Dropout
+from keras.models import Model, load_model, Sequential
 from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 from keras.optimizers import Adamax
-from skimage.color import rgb2lab
+from keras.utils import to_categorical
 from math import ceil
+import numpy
 import keras
 import os
 
 
 class NeuralNetwork(object):
-    def __init__(self, training_path, epochs=10, batch_size=1, path_to_model=None):
-        self.training_path = training_path
+    def __init__(self, epochs=10, batch_size=1, path_to_model=None):
 
-        self.training_set_size = 0
-        for direct in [dirname for dirname in os.listdir(self.training_path)]:
-            self.training_set_size += len(
-                [filename for filename in os.listdir(self.training_path + "/" + direct) if filename.endswith(".png")])
         self.epochs = epochs
         self.batch_size = batch_size
-        self.datagen = ImageDataGenerator(shear_range=0.2, zoom_range=0.2, rotation_range=20, horizontal_flip=True)
+        with open("x_batch_16.txt","r") as x, open("y_batch_16.txt",'r') as y:
+            self.x_batch = [ [ [ord(ch)-96] for ch in line.rstrip() ] for line in x ]
+            self.y_batch = [ [ [ord(ch)-96] for ch in line.rstrip() ] for line in y ]
+            self.training_set_size = len(self.x_batch)
+
+        self.x_batch = to_categorical(numpy.array(self.x_batch))
+        self.y_batch = to_categorical(numpy.array(self.y_batch))
+
+        #print(self.x_batch[0])
+        #print(self.x_batch[1])
+
+        #print(self.y_batch[0])
+        #print(self.y_batch[1])
+
         if path_to_model is None:
-            self.model = NeuralNetwork.neural_network_structure()
+            self.neural_network_structure()
         else:
-            self.model = NeuralNetwork.load_model_from_file(path_to_model)
+            self.model = load_model(path_to_model)
 
-    @staticmethod
-    def neural_network_structure():
-        network_input = Input(shape=(256, 256, 1,))
+    def neural_network_structure(self):
 
-        # encoder
+        self.model = Sequential()
 
-        network = Conv2D(16, (3, 3), activation='relu', padding='same')(network_input)
-        network = Conv2D(16, (3, 3), activation='tanh', padding='same')(network_input)
-        network = Conv2D(16, (3, 3), activation='relu', padding='same')(network_input)
+        #encoder - begin
 
-        network = MaxPooling2D((2, 2))(network)
-        network = BatchNormalization()(network)
+        self.model.add(Conv1D(filters=32,kernel_size=3,activation='relu', padding='same',input_shape=(16,27)))
+        self.model.add(Conv1D(filters=32,kernel_size=3,activation='tanh', padding='same'))
+        self.model.add(Conv1D(filters=32,kernel_size=3,activation='relu', padding='same'))
 
-        network = Conv2D(32, (3, 3), activation='relu', padding='same')(network)
-        network = Conv2D(32, (3, 3), activation='tanh', padding='same')(network)
-        network = Conv2D(32, (3, 3), activation='relu', padding='same')(network)
-        network = MaxPooling2D((2, 2))(network)
-        network = BatchNormalization()(network)
+        self.model.add(BatchNormalization())
+        self.model.add(Dropout(0.3))
 
-        network = Conv2D(64, (3, 3), activation='relu', padding='same')(network)
-        network = Conv2D(64, (3, 3), activation='tanh', padding='same')(network)
-        network = Conv2D(64, (3, 3), activation='relu', padding='same')(network)
-        network = MaxPooling2D((2, 2))(network)
-        network = BatchNormalization()(network)
+        self.model.add(Conv1D(filters=64,kernel_size=3,activation='relu', padding='same'))
+        self.model.add(Conv1D(filters=64,kernel_size=3,activation='tanh', padding='same'))
+        self.model.add(Conv1D(filters=64,kernel_size=3,activation='relu', padding='same'))
+       
+        self.model.add(BatchNormalization())
+        self.model.add(Dropout(0.3))
 
-        network = Conv2D(128, (3, 3), activation='relu', padding='same')(network)
-        network = Conv2D(128, (3, 3), activation='tanh', padding='same')(network)
-        network = Conv2D(128, (3, 3), activation='relu', padding='same')(network)
-        network = MaxPooling2D((2, 2))(network)
-        network = BatchNormalization()(network)
+        self.model.add(Conv1D(filters=128,kernel_size=3,activation='relu', padding='same'))
+        self.model.add(Conv1D(filters=128,kernel_size=3,activation='tanh', padding='same'))
+        self.model.add(Conv1D(filters=128,kernel_size=3,activation='relu', padding='same'))
 
-        # decoder
+        self.model.add(BatchNormalization())
 
-        network = Conv2D(256, (3, 3), activation='relu', padding='same')(network)
-        network = Conv2D(256, (3, 3), activation='tanh', padding='same')(network)
-        network = Conv2D(256, (3, 3), activation='relu', padding='same')(network)
-        network = UpSampling2D((2, 2))(network)
-        network = BatchNormalization()(network)
+        #encoder- end
 
-        network = Conv2D(128, (3, 3), activation='relu', padding='same')(network)
-        network = Conv2D(128, (3, 3), activation='tanh', padding='same')(network)
-        network = Conv2D(128, (3, 3), activation='relu', padding='same')(network)
-        network = UpSampling2D((2, 2))(network)
-        network = BatchNormalization()(network)
+        #decoder - begin
 
-        network = Conv2D(64, (3, 3), activation='relu', padding='same')(network)
-        network = Conv2D(64, (3, 3), activation='tanh', padding='same')(network)
-        network = Conv2D(64, (3, 3), activation='relu', padding='same')(network)
-        network = UpSampling2D((2, 2))(network)
-        network = BatchNormalization()(network)
+        self.model.add(Conv1D(filters=128,kernel_size=3,activation='relu', padding='same'))
+        self.model.add(Conv1D(filters=128,kernel_size=3,activation='tanh', padding='same'))
+        self.model.add(Conv1D(filters=128,kernel_size=3,activation='relu', padding='same'))
 
-        network = Conv2D(32, (3, 3), activation='relu', padding='same')(network)
-        network = Conv2D(32, (3, 3), activation='tanh', padding='same')(network)
-        network = Conv2D(32, (3, 3), activation='relu', padding='same')(network)
-        network = UpSampling2D((2, 2))(network)
-        network = BatchNormalization()(network)
+        self.model.add(BatchNormalization())
+        self.model.add(Dropout(0.3))
 
-        network = Conv2D(16, (3, 3), activation='relu', padding='same')(network)
-        network = Conv2D(16, (3, 3), activation='tanh', padding='same')(network)
-        network = Conv2D(16, (3, 3), activation='relu', padding='same')(network)
-        network = BatchNormalization()(network)
+        self.model.add(Conv1D(filters=64,kernel_size=3,activation='relu', padding='same'))
+        self.model.add(Conv1D(filters=64,kernel_size=3,activation='tanh', padding='same'))
+        self.model.add(Conv1D(filters=64,kernel_size=3,activation='relu', padding='same'))
 
-        network = Conv2D(4, (3, 3), activation='relu', padding='same')(network)
-        network = Conv2D(4, (3, 3), activation='tanh', padding='same')(network)
-        network = Conv2D(4, (3, 3), activation='relu', padding='same')(network)
-        network_output = Conv2D(2, (3, 3), activation='tanh', padding='same')(network)
+        self.model.add(BatchNormalization())
+        self.model.add(Dropout(0.3))
 
-        return Model(inputs=network_input, outputs=network_output)
+        self.model.add(Conv1D(filters=32,kernel_size=3,activation='relu', padding='same'))
+        self.model.add(Conv1D(filters=32,kernel_size=3,activation='tanh', padding='same'))
+        self.model.add(Conv1D(filters=32,kernel_size=3,activation='relu', padding='same'))
 
-    @staticmethod
-    def load_model_from_file(filename):
-        return load_model(filename)
+        #decoder - end 
+        
+        self.model.add(Dense(27,activation='softmax'))
 
-    def image_a_b_gen(self):
-
-        for batch in self.datagen.flow_from_directory(self.training_path, batch_size=self.batch_size):
-            _batch = (1.0 / 255) * batch[0]
-            lab_batch = rgb2lab(_batch)
-            x_batch = lab_batch[:, :, :, 0] / 512
-            x_batch = x_batch.reshape(x_batch.shape + (1,))
-            y_batch = lab_batch[:, :, :, 1:] / 512
-            yield (x_batch, y_batch)
+        self.model.compile(optimizer=Adamax(lr=0.001), loss='categorical_crossentropy', metrics=['mae', 'acc'])
+ 
 
     def train(self):
-        # tensorboard --logdir=path/to/log-directory
-        opt = Adamax(lr=0.001)
-        tb_callback = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=self.batch_size,
-                                                  write_graph=True,
+        patience = 10
+        tb_callback = keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=self.batch_size, write_graph=True,
                                                   write_grads=False, write_images=False, embeddings_freq=0,
                                                   embeddings_layer_names=None, embeddings_metadata=None)
-        self.model.compile(optimizer=opt, loss='mse', metrics=['mae', 'acc'])
-        self.model.fit_generator(self.image_a_b_gen(), epochs=self.epochs,
-                                 steps_per_epoch=int(ceil(float(self.training_set_size) / self.batch_size)),
-                                 callbacks=[tb_callback])
+        model_names = 'model.{epoch:02d}-{loss:.10f}.hdf5'
+        model_checkpoint = ModelCheckpoint(os.path.join('models', model_names), monitor='loss', verbose=1, save_best_only=True)
+        early_stop = EarlyStopping('loss', patience=patience)
+        reduce_lr = ReduceLROnPlateau('loss', factor=0.1, patience=int(patience / 2), verbose=1)
+
+
+        self.model.fit(self.x_batch,self.y_batch, batch_size=self.batch_size, epochs=self.epochs,callbacks=[tb_callback, early_stop, reduce_lr, model_checkpoint])
 
     def save_model(self):
-        self.model.save_weights('weights_{}e_pic.h5'.format(self.epochs))
-        self.model.save('model_{}e_pic_m.h5'.format(self.epochs))
+        self.model.save_weights('weights_{}e.h5'.format(self.epochs))
+        self.model.save('model_{}e_m.h5'.format(self.epochs))
 
     def run(self):
         self.train()
---
+++ tools.py
@@ -0,0 +1,115 @@
+from datamuse import datamuse
+from keras.utils import plot_model
+from keras.models import load_model
+from keras.utils import to_categorical
+import numpy
+import argparse
+import time
+
+def downloadData():
+    api = datamuse.Datamuse()
+    try:
+        words = open("words_alpha.txt", encoding='utf-8')
+        rhymes = open("rhymes.txt", 'a', encoding='utf-8')
+        progress = open("progress.txt", 'r', encoding='utf-8')
+
+        line_no = int(progress.readline())
+        progress.close()
+        line_begin = line_no
+        print(line_no)
+        for i, line in enumerate(words):
+            if (i == line_no):
+                if (line_no < line_begin + 75000):
+                    line = line.rstrip()
+                    try:
+                        api_rhymes = api.words(rel_rhy=line, max=5)
+                        [rhymes.write(line+';'+rh["word"]+'\n')
+                            for rh in api_rhymes]
+                        print(line)
+                    except:
+                        print("json decode on " + line)
+                    
+                    if len(api_rhymes) < 5:
+                        try:
+                            near_rhymes = api.words(rel_nry=line, max=5)
+                            [rhymes.write(line+';'+rh["word"]+'\n')
+                                for rh in near_rhymes]
+                            print(line)
+                        except:
+                            print("json decode on " + line)
+                    rhymes.flush()
+                    line_no = line_no+1
+                else:
+                    break
+    finally:
+        progress = open("progress.txt", 'w', encoding='utf-8')
+        progress.write(str(line_no))
+        words.close()
+        rhymes.close()
+        progress.close()
+
+def split_exact(size):
+    with open("rhymes.txt",'r') as rhymes, open(f"x_batch_{size}_exact.txt",'w') as x_batch, open(f"y_batch_{size}_exact.txt",'w') as y_batch:
+        for line in rhymes:
+            x, y = map(lambda x: x.strip(), line.split(';'))
+            if len(x) != size or len(y) != size:
+                continue
+            x_batch.write(x.rjust(size,'`')+'\n')
+            y_batch.write(y.rjust(size,'`')+'\n')
+
+def split(size):
+    with open("rhymes.txt",'r') as rhymes, open(f"x_batch_{size}.txt",'w') as x_batch, open(f"y_batch_{size}.txt",'w') as y_batch:
+        for line in rhymes:
+            x, y = map(lambda x: x.strip(), line.split(';'))
+            if len(x) > size or len(y) > size:
+                continue
+            x_batch.write(x.rjust(size,'`')+'\n')
+            y_batch.write(y.rjust(size,'`')+'\n')
+
+def predict(path_to_model):
+    model = load_model(path_to_model)
+
+    with open("to_predict.txt",'r') as to_predict, open("predicted.txt",'w') as predicted:
+        to_predict = [line.rstrip().lower().rjust(16,'`') for line in to_predict]
+        to_predict = [[ [ord(ch)-96] for ch in line] for line in to_predict]
+        to_predict = to_categorical(numpy.array(to_predict),27)
+        result = model.predict(to_predict)
+        for line in result:
+            for c in line:
+                if numpy.argmax(c) == 96:
+                    c = 32
+                else:
+                    c = numpy.argmax(c)+96
+                predicted.write(chr(c))
+            predicted.write('\n')
+        
+
+def saveGraphKeras(modelName):
+    model = load_model(modelName)
+    plot_model(model, to_file='model.png',show_shapes=True,show_layer_names=False)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='parser')
+    parser.add_argument('-m', help='predict with the given model')
+    parser.add_argument('-g', help='graph the given model')
+    parser.add_argument('-d', help='download data')
+    parser.add_argument("--split", type=int, help="split rhymes")
+    parser.add_argument("--split_exact", type=int, help="split rhymes exact")
+    parser.add_argument("--download", help="download data")
+
+    args = parser.parse_args()
+
+    if args.m:
+        predict(args.m)
+
+    if args.g:
+        saveGraphKeras(args.g)
+
+    if args.split:
+        split(args.split)
+
+    if args.split_exact:
+        split(args.split_exact)
+
+    if args.download:
+        downloadData()
